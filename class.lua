e08f820b654036b95d036b0048fcb73ba5b99eae
thabubble <thabubble@gmail.com>
2015-05-23 15:28:49 +0200

    Better inheritance and call handling

--- class.lua
+++ class.lua
@@ -1,100 +1,88 @@
 function class(def)
     local class = {}
     local parents = {}
-    setmetatable(class, class)
     
+    local wraps
     local function super(parent_class)
         if not parent_class then
             parent_class = parents[1]
-            
-            return setmetatable({}, {
-                __ipairs    = function()        return ipairs(parent_class)              end,
-                __pairs     = function()        return  pairs(parent_class)              end,
-                __index     = function(t, name) return        parent_class[name]         end,
-                __index_new = function(t, name, value)        parent_class[name] = value end
-            })
         end
+        
+        local this = this
+        local that = {}
+        for k,v in pairs(parent_class) do
+            that[k] = type(v) == 'function' and wraps(this, v) or v
+        end
+        
+        return setmetatable(that, that)
     end
     
-    function class.__init(...) end
-    function class.__call(...)
-        local this = {}
-        for k,v in pairs(class) do
-            if type(v) == "function" then
-                v = (function(func)
-                    return function(...)
-                        local t = _ENV.this
-                        local s = _ENV.super
-                        
-                        _ENV.this  = this
-                        _ENV.super = super
-                        
-                        local ret  = func(...)
-                        
-                        _ENV.this  = t
-                        _ENV.super = s
-                        
-                        return ret
-                    end
-                end)(v)
+    function wraps(this, func)
+        return function(...)
+                local t = _ENV.this
+                local s = _ENV.super
+                
+                _ENV.this  = this
+                _ENV.super = super
+                
+                local ret  = func(...)
+                
+                _ENV.this  = t
+                _ENV.super = s
+                
+                return ret
             end
-            this[k] = v
-        end
-        this.__class = class
-        this.__init(...)
-        return this
     end
+    
+    function class.__init() end
+    local class_wrap = setmetatable({}, {
+        __ipairs    = function()        return ipairs(class)              end,
+        __pairs     = function()        return  pairs(class)              end,
+        __index     = function(t, name) return        class[name]         end,
+        __index_new = function(t, name, value)        class[name] = value end,
+        __call      = function(...)
+            local this = {}
+            for k,v in pairs(class) do
+                this[k] = type(v) == 'function' and wraps(this, v) or v
+            end
+            
+            this.__class = class
+            this.__init(...)
+            
+            return setmetatable(this, this)
+        end
+    })
 
     for i=1,math.huge do
         inherit, v = debug.getlocal(def, i)
         if not inherit then break end
         
-        local parent_class = _G[inherit]
-        for i=1,math.huge do
-            local name, pclass = debug.getlocal(2,i,1)
-            if not name then break
-            elseif name == inherit then
-                parent_class  = pclass
-                break
-            end
-        end
-        
+        local parent_class = _ENV[inherit]
         if parent_class  and type(parent_class) == 'table' then
             table.insert(parents, parent_class)
             for k,v in pairs(parent_class) do
-                if k ~= "__call" then
-                    class[k] = v
-                end
+                class[k] = v
             end
         else
             error(string.format('Class "%s" not valid.', name))
         end
     end
-    
-    class.__parents = parents
 
     local env = _ENV
     _ENV = setmetatable({}, {
         __index= function(t, name)
-            local value = class[name]
-            
-            if value == nil then
-                return env[name]
-            else
-                return value
-            end
+            local  value  = class[name]
+            return value ~= nil and value or env[name]
         end,
-        __newindex =
-            function(t, name, value)
-                if name ~= "__call" then
-                    class[name] = value
-                end
+        __newindex = function(t, name, value)
+                class[name] = value
             end,
     })
-    env.pcall(def)
+    
+    env.pcall(def, env.table.unpack(parents))
     _ENV = env
     
-    return class
+    return class_wrap
 end
 
 global  = true
@@ -104,6 +92,10 @@ Inherit = class(function()
     function __init()
         print('Inherit().__init()')
         this.init = true
+    end
+    
+    function __call()
+        print('Yay! You\'re calling for me :) init:', this.init, '\n')
     end
 end)
 
@@ -124,19 +116,21 @@ end)
 
 example = Example()
 example.test('__init:')
+example()
 
 example.property = 'I\'m a property of instance "example"'
 print('example.property', example.property)
 print('Example.property', Example.property)
 
---Output:
---  Inherited property:	true
---  Global variable:   	true	
+--    Inherited property:	true
+--    Global variable:   	true	
 
---  Example().__init()
---  Inherit().__init()
---  this.init:	true
---  __init:	function: 0x1e68170	
+--    Example().__init()
+--    Inherit().__init()
+--    this.init:	true
+--    __init:	function: 0x15dd5f0	
 
---  example.property	I'm a property of instance "example"
---  Example.property	nil
+--    Yay! You're calling for me :) init:	true	
+
+--    example.property	I'm a property of instance "example"
+--    Example.property	nil
